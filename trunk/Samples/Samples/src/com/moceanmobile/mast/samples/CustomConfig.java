57d874e98f398d6e300a2b671aeb2b90fa745ea8
jdickert.cellavant@gmail.com <jdickert.cellavant@gmail.com@f3a8e223-0ac0-4266-b396-771bc5b56a4e>
2013-09-03 22:57:37 +0000

    Adding Samples icon and updating CustomConfig to be a simple refresh sample until a new custom dialog is built.

--- trunk/Samples/Samples/src/com/moceanmobile/mast/samples/CustomConfig.java
+++ trunk/Samples/Samples/src/com/moceanmobile/mast/samples/CustomConfig.java
@@ -1,22 +1,12 @@
 package com.moceanmobile.mast.samples;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 
-public class CustomConfig extends Activity {
+public class CustomConfig extends RefreshActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_custom_config);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.custom_config, menu);
-		return true;
-	}
-
 }
